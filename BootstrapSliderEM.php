e002347515510ab2520ea4518cae00b00c1b6785
Sumon Chattopadhyay <sumon.chattopadhyay@utah.edu>
2022-06-06 15:52:28 -0700

    Adds initial class and config.json file

---
+++ BootstrapSliderEM.php
@@ -0,0 +1,8 @@
+<?php
+namespace Utah\BootstrapSliderEM;
+
+class BootstrapSliderEM extends \ExternalModules\AbstractExternalModule {
+	public function __construct() {
+		parent::__construct();
+		// Other code to run when object is instantiated
+	}}
