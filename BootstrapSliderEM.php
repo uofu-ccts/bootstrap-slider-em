6eb1254751ea513b43c1518f2af65acf48c6ddeb
Sumon Chattopadhyay <sumon.chattopadhyay@utah.edu>
2022-06-07 11:23:42 -0700

    Adds hooks and bootstrapSlider method to class file

--- BootstrapSliderEM.php
+++ BootstrapSliderEM.php
@@ -2,7 +2,19 @@
 namespace Utah\BootstrapSliderEM;
 
 class BootstrapSliderEM extends \ExternalModules\AbstractExternalModule {
-	public function __construct() {
-		parent::__construct();
-		// Other code to run when object is instantiated
-	}}
+
+  function redcap_survey_page_top($project_id, $record, $instrument)
+  {
+    $this->bootstrapSlider();
+  }
+
+  function redcap_data_entry_form_top($project_id, $record, $instrument)
+  {
+    $this->bootstrapSlider();
+  }
+
+  function bootstrapSlider()
+  {
+    
+  }
+}
